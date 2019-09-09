3fae25762f71fe7e88182520cf11529250f8f0c6
Daisy I Ogholo <dogholo@beyster1620p03.engin.umich.edu>
2019-09-09 16:47:36 -0400

    I tried Python

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print("My name is Daisy")
+import random
+r1 = random.randint(1.100)
+print(r1)
+r2 = random.randint(1.100)
+print(r2)
+Sum = r1+r2
+print("Sum = " )
+print(Sum)
+print("Average = " )
+Sum = Sum / 2
+print(Sum)
