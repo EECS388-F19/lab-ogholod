39ea1ca74d5204d8a5de29c6faf83b20de92e619
dogholo <dogholo@umich.edu>
2019-09-11 20:56:40 -0400

    Victory is Mine

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,9 @@
 print("My name is Daisy")
 import random
-r1 = random.randint(1.100)
+r1 = random.randint(1,100)
 print(r1)
-r2 = random.randint(1.100)
+r2 = random.randint(1,100)
 print(r2)
 Sum = r1+r2
-print("Sum = " )
-print(Sum)
-print("Average = " )
-Sum = Sum / 2
-print(Sum)
+print("Sum = ", Sum)
+print("Average = ", Sum/2)
